ca1037175d741b37746412b96cfffcecb76fe79e
Martin Gamnitzer <kontakt@martin-gamnitzer.de>
2011-01-18 22:50:46 +0100

    two bugfixes, logging added

--- app.rb
+++ app.rb
@@ -6,9 +6,34 @@ require 'active_record'
 load 'config.rb'
 load 'models.rb'
 
+# Define an error class
+#class UsageError < Exception
+#end
+#error UsageError do
+#	'Sorry, this is not allowd'
+#end
+
+
+# helpers
+helpers do
+	def logger(token,tabelle)
+time = Time.now()
+		log = Apilogging.new(:single_access_token => token, :tabelle => tabelle, :ip => request.env['REMOTE_ADDR'], :timestamp => time.strftime("%Y-%m-%d %H:%M:%S"))
+		log.save(false)
+	end
+end
+
+# the routes
+get '/' do
+  'Sorry, this is not allowed'
+end
+get '/:model' do
+  'Sorry, this is not allowed'
+end
 
 # all models (API tables) belong here
 get '/:model/' do
+logger(params[:key],params[:model])
 
 # get user
 user = User.find(:first, :conditions => [ "single_access_token = ?", params[:key]])
@@ -19,7 +44,7 @@ if user == nil
 end
 
 # get permissions
-permissions = Permission.find(:all, :joins=> :users, :conditions => {:task => "read", :table => params[:model], :users => { :id => user.id } }) 
+permissions = Permission.find(:all, :joins=> :users, :conditions => {:access => "read", :tabelle => params[:model], :users => { :id => user.id } }) 
 if permissions.size == 0
 	abort("no permissions found")
 end
@@ -27,7 +52,7 @@ end
 # create select based on permissions
 columns = Array.new()
 permissions.each do |per|
-	columns += [per.column]
+	columns += [per.spalte]
 end
 
 ##### optional parameters #####
@@ -40,11 +65,12 @@ if params[:id]!=nil
 end
 
 ##### fetch data #####
-Datatable.set_table_name("data_#{params[:model]}")
-results = Datatable.find(:all, :select => columns, :limit => limit, :conditions => where)
+# Clone Datatable to have a fresh Object every time
+Datahelper = Datatable.clone()
+Datahelper.set_table_name("data_#{params[:model]}")
+results = Datahelper.find(:all, :select => columns, :limit => limit, :conditions => where)
 
 # create json from result
-# FIXME: Why is "datatable" in JSON? How to remove it?
 "#{results.to_json()}"
 
 end
--- models.rb
+++ models.rb
@@ -1,8 +1,11 @@
+ActiveRecord::Base.include_root_in_json = false # removes table names in json
 class User < ActiveRecord::Base
 	has_and_belongs_to_many :permissions
 end
 class Permission < ActiveRecord::Base
 	has_and_belongs_to_many :users
+end
+class Apilogging < ActiveRecord::Base
 end
 class Datatable < ActiveRecord::Base # no real Table, just used for Data-Tables
 end
