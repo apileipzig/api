59c964b35e9ecfcbef1f752d292db87c79a79fae
Martin Gamnitzer <kontakt@martin-gamnitzer.de>
2011-01-18 17:59:45 +0100

    changed column names in permissions table

--- db/permissions.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `permissions` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `task` varchar(255) DEFAULT NULL,
-  `table` varchar(255) DEFAULT NULL,
-  `column` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8
