29ac89e897940cc28c4cd875aaf980e4de1a4174
ben <ben@nerdlabor.de>
2011-01-24 22:57:13 +0100

    encapsulated functionality for DRY, added rest style interface, added methods for validation, error handling, output, cleaned up

--- app.rb
+++ app.rb
@@ -6,100 +6,116 @@ require 'config.rb'
 require 'models.rb'
 require 'helpers.rb'
 
+#TODO:
+#add config.yml / ensure that there is a db connection
+#do paging
+#handle put, post, delete requests ('/:model/:id')
+##validate which columns can be changed / are needed
+#handle parametrized requests (do this in a generic way, which means allow every parameter which is a table column of the model too)
+##add more parameters like limit
 
-error 403 do
-	"#{Hash['error'=>'No permissions found'].to_json()}"
-end
-
-error 401 do
-	"#{Hash['error'=>'Authentication failed'].to_json()}"
-end
-
-error 404 do
-	"#{Hash['error'=>'No valid route'].to_json()}"
-end
-
-error 405 do
-	"#{Hash['error'=>'wrong parameter format'].to_json()}"
-end
-
-
-# the routes
 get '/' do
-	error 404
+	#TODO: throw other error her, maybe a help message
+	throw_error 405
 end
 
-# all models (API tables) belong here
-get '/:model/:action' do
+#request a list in rest/rails style
+get '/:model' do
 	logger(params[:key],params[:model])
+	validate params
+	Datatable.set_table_name("data_#{params[:model]}")
+	output Datatable.find(:all, :select => only_permitted_columns, :limit => params[:limit]), params[:format]
+end
 
-	if ![ "create", "read", "update", "delete" ].include?(params[:action])
-		error 404
-	end
+#per model requests
+#create
+post '/:model/:id' do
+	#TODO
+end
+
+#read
+get '/:model/:id' do
+	#TODO
+end
+
+#update
+put '/:model/:id' do
+	#TODO
+end
+
+#delete
+delete '/:model/:id' do
+	#TODO
+end
+
+private
+
+#validating every request
+#TODO: validate if model exists (or let the error "No permission(s) to do this." for now)
+def validate params
+
+	throw_error 401 if params[:key].nil?
+	throw_error 405 unless params[:key].match(/^[A-Za-z0-9]*$/)
+	throw_error 405 unless params[:model].match(/^[A-Za-z0-9]*$/)
 	
-	# check token
-	error 401 if !params[:key].match(/^[A-Za-z0-9]*$/)	#TODO: Limit token length?
-	# get user
-	user = User.find(:first, :conditions => [ "single_access_token = ?", params[:key]])
-	# check if user exists
-	error 401 if user.nil?
-
-	# regex table
-	error 405 if !params[:model].match(/^[A-Za-z0-9]*$/)
-	# get permissions
-	permissions = Permission.find(:all, :joins=> :users, :conditions => {:access => params[:action], :tabelle => params[:model], :users => { :id => user.id } }) 
-	if permissions.size == 0
-		error 403
-	end
-
-	# TODO: Split code into different functions depending on :action
-
-	# check for paramters like size or columns
-	if params[:size]	# returns length of table
-		Datahelper = Datatable.clone()
-		Datahelper.set_table_name("data_#{params[:model]}")
-		results = Hash["#{params[:model]} size", Datahelper.count]
-	elsif params[:columns] #return an array with columns
-		results = Array.new()
-		permissions.each do |permission|
-			results += [permission.spalte]
-		end
-	else
-		# create select based on permissions
-		columns = Array.new()
-		permissions.each do |per|
-			columns += [per.spalte]
-		end
-
-		##### optional parameters #####
-		# limit
-		if params[:limit]!=nil
-			error 405 if !params[:limit].to_s.match(/^[0-9]*$/)
-			limit = params[:limit].to_i;
-		else
-			limit = 10
-		end
-		
-		where = Hash.new()
-		# id (one or comma separated)
-		
-		if params[:id]!=nil
-			error 405 if !params[:id].to_s.match(/^[0-9,]*$/)
-			where["id"] = params[:id].split(',')#to_i
-		end
-
-		##### fetch data #####
-		# Clone Datatable to have a fresh Object every time
-		Datahelper = Datatable.clone()
-		Datahelper.set_table_name("data_#{params[:model]}")
-		results = Datahelper.find(:all, :select => columns, :limit => limit, :conditions => where)
-		
-	end
 	
-	# create json or xmlfrom result
-	if params[:output] == "xml"
-		"#{results.to_xml(:skip_instruct => false, :skip_types => true)}"
-	else
-		"#{results.to_json()}"
+	throw_error 405 unless params[:limit].match(/^[0-9]*$/) unless params[:limit].nil?
+	params[:limit] = 10 if params[:limit].to_i > 10 unless params[:limit].nil?
+
+	@user = User.find(:first, :conditions => [ "single_access_token = ?", params[:key]])
+	throw_error 401 if @user.nil?
+	
+	#TODO: connect permissions and user through the models (rails style)
+	@permissions = Permission.find(:all, :joins=> :users, :conditions => {:access => get_action(request.env['REQUEST_METHOD']), :tabelle => params[:model], :users => { :id => @user.id } })
+	throw_error 403 if @permissions.empty?
+end
+
+#error handling
+def throw_error code
+	#TODO: add more output information here, maybe a help message
+	halt code, output({"error" => "Authentication failed"}) if code == 401
+	halt code, output({"error" => "No permission(s) to do this."}) if code == 403
+	#TODO: add which parameter is wrong or missing
+	halt code, output({"error" => "wrong parameter format"}) if code == 405
+end
+
+#map request_method to db access names
+def get_action request_method
+	case request_method
+		when 'POST': 'create'
+		when 'GET': 'read'
+		when 'PUT': 'update'
+		when 'DELETE': 'delete'
 	end
 end
+
+#generate resulting output
+#output data
+#output data format
+def output *args
+	if args[1] == "xml"
+		content_type 'text/xml', :charset => 'utf-8'
+		"#{args[0].to_xml(:skip_instruct => false, :skip_types => true)}"
+	else
+		#ensure that result is a json object everytime, not a json object or an array of json objects (like facebook)
+		#FIXME: do this in a better and more reliable way
+		h = Hash.new
+		if args[0].is_a?(Array)
+			h['data'] = args[0]
+		else 
+			h = args[0]
+		end
+		content_type 'text/plain', :charset => 'utf-8'
+		"#{h.to_json()}"
+	end
+end
+
+#specify only the colums we have rights to
+def only_permitted_columns
+	columns = Array.new()
+	@permissions.each do |per|
+		columns += [per.spalte]
+	end
+	columns
+end
+
