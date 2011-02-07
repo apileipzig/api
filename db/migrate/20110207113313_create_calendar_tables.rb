a490ee9b639be7a32b6975c235dabeb65b9ee7c4
ben <ben@nerdlabor.de>
2011-02-07 12:00:23 +0100

    added calendar migration and models, fixed bug with company branch join table name

---
+++ db/migrate/20110207113313_create_calendar_tables.rb
@@ -0,0 +1,45 @@
+class CreateDataTables < ActiveRecord::Migration
+  def self.up
+    create_table :data_calendar_events do |t|
+			t.references	:category								# Kategorie des Events (ist sub_market)
+			t.references	:host_id
+      t.date 				:date_from
+      t.time 				:time_from
+      t.date 				:date_to
+      t.time 				:time_to
+			t.string			:name										# Name des Events
+			t.text				:description						# Beschreibung des Events
+			t.string			:url										# eine primäre Url beginnend mit http://
+			t.timestamps
+    end
+
+    create_table :data_calendar_venues do |t|
+			t.string			:name										# Name des Veranstaltungsortes
+			t.string			:street									# offizieller Straßenname, "-Straße" ausschreiben!
+			t.integer			:housenumber						# nur EINE Zahl, für alles weitere das Feld hausnummer_zusatz verwenden
+			t.string			:housenumber_additional	# Zusatz zur Hausnummer: z.B. -45, a, Tor B, Gebäude 5, Aufgang G
+			t.string			:postcode								# fünfstellige Postleitzahl (String, keine Zahl siehe http://de.wikipedia.org/wiki/Postleitzahl#Postleitzahlen_in_der_Datenverarbeitung)
+			t.string 			:city										# offizieller Name des Ortes, Halle (Saale) -> korrekt, Halle an der Saale -> falsch
+			t.string			:phone									# eine primäre Rufummer nach DIN 5008 (http://de.wikipedia.org/wiki/Rufnummer#Schreibweisen)
+			t.text				:description						# Beschreibung des Veranstaltungsortes
+			t.string			:url										# eine primäre Url beginnend mit http://
+			t.timestamps
+    end
+
+    create_table :calendar_hosts do |t|
+			t.string			:first_name
+			t.string			:last_name
+			t.string			:phone									# eine primäre Rufummer nach DIN 5008 (http://de.wikipedia.org/wiki/Rufnummer#Schreibweisen)
+			t.string			:mobile_primary					# siehe phone
+			t.string			:url										# eine primäre Url beginnend mit http://
+			t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table	:data_calendar_events
+		drop_table	:data_calendar_venues
+		drop_table	:calendar_hosts
+  end
+end
+
--- lib/models.rb
+++ lib/models.rb
@@ -13,17 +13,25 @@ end
 class RequestLog < ActiveRecord::Base
 end
 
+class TempSync < ActiveRecord::Base
+end
+
+##############
+#Mediahandbook
+##############
+
 class Company < ActiveRecord::Base
 	set_table_name "data_mediahandbook_companies"
 	belongs_to :sub_market, :class_name => "Branch"
 	belongs_to :main_branch, :class_name => "Branch"
-	has_and_belongs_to_many :sub_branches, :class_name => "Branch", :limit => 6
+	has_and_belongs_to_many :sub_branches, :class_name => "Branch", :limit => 6, :join_table => "mediahandbook_branches_companies"
 	has_many :people
 	#TODO maybe we need a validation for creating 6 sub_branches max
 end
 
 class Branch < ActiveRecord::Base
 	set_table_name "data_mediahandbook_branches"
+	has_and_belongs_to_many :companies, :join_table => "mediahandbook_branches_companies"
 end
 
 class Person < ActiveRecord::Base
@@ -31,6 +39,21 @@ class Person < ActiveRecord::Base
 	belongs_to :company
 end
 
-class TempSync < ActiveRecord::Base
+#########
+#Calendar
+#########
+
+class Event < ActiveRecord::Base
+	belongs_to :category, :class_name => "Branch"
+	belongs_to :host
+	has_one :venue
+end
+
+class Venue < ActiveRecord::Base
+	belongs_to :event
+end
+
+class Host < ActiveRecord::Base
+	has_many :events
 end
 
