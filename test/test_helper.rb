5b7eafd3306ce8fcb785e02221c58b7504d15469
Lars Henrik Mai <lars.mai@kontinui.de>
2011-11-18 22:40:35 +0100

    setup database_cleaner for tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,17 +7,37 @@ require 'api'
 require 'test/unit'
 require 'assert_json'
 require 'rack/test'
-require 'turn'
+# require 'turn'
 require 'shoulda-context'
 require 'assertions'
 require 'factory_girl'
 require 'authlogic/test_case'
 require 'factories'
+require 'database_cleaner'
+
 
 class Test::Unit::TestCase
   include Rack::Test::Methods
   include AssertJson
   include Api::Assertions
+
+  class << self
+    def startup
+      DatabaseCleaner.strategy = :transaction
+      DatabaseCleaner.clean_with(:truncation)
+    end
+
+    def shutdown
+    end
+  end
+
+  def setup
+    DatabaseCleaner.start
+  end
+
+  def teardown
+    DatabaseCleaner.clean
+  end
 
   def app
     Sinatra::Application
