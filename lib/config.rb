70527e1c4e5a2ec3378d3f70ec31cd10f4c15e21
ben <ben@nerdlabor.de>
2012-03-09 01:36:39 +0100

    added heroku db stuff

--- lib/config.rb
+++ lib/config.rb
@@ -16,11 +16,21 @@ end
 
 begin
   db_config = YAML.load_file(APP_ROOT + '/database.yml')[settings.environment.to_s]
+  ActiveRecord::Base.establish_connection(db_config)
 rescue
-  db_config = URI.parse(ENV['DATABASE_URL'])
+  db = URI.parse(ENV['DATABASE_URL'])
+
+  ActiveRecord::Base.establish_connection(
+    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
 end
 
-ActiveRecord::Base.establish_connection(db_config)
+
 
 require APP_ROOT + '/lib/models.rb'
 require APP_ROOT + '/lib/helpers.rb'
