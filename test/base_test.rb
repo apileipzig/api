6c5d15ed8b8aee04264820f450a0b51d438d9811
benben <ben@nerdlabor.de>
2012-05-08 13:18:17 +0200

    added error message for another base route and test for it

--- test/base_test.rb
+++ test/base_test.rb
@@ -12,4 +12,16 @@ class BaseTest < Test::Unit::TestCase
     end
   end
 
+  context "GET '/some_source/'" do
+    setup do
+      @api_user = FactoryGirl.create(:user)
+      get '/mediahandbook/'
+    end
+
+    should "return an error" do
+      assert_status 400
+      assert_body { |json| json.element "error", "Wrong url format." }
+    end
+  end
+
 end
